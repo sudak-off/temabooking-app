7b81c79118410ab5409008d41d96284fe2dc743c
Artyom <sudakov.artyom@gmail.com>
2022-06-22 16:23:09 +0300

    hotfix bag

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -10,11 +10,13 @@ import "./header.scss";
 import { useState } from "react";
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
+import { useContext } from "react";
+import { SearchContext } from "../../context/SearchContext";
 
 export const Header = ({ type }) => {
     const [destination, setDestination] = useState("");
     const [openDate, setOpenDate] = useState(false);
-    const [date, setDate] = useState([
+    const [dates, setDates] = useState([
         {
             startDate: new Date(),
             endDate: new Date(),
@@ -39,8 +41,11 @@ export const Header = ({ type }) => {
         });
     };
 
+    const { dispatch } = useContext(SearchContext)
+
     const handleSearch = () => {
-        navigate("/hotels", { state: { destination, date, options } });
+        dispatch({ type: "NEW_SEARCH", payload: { destination, dates, options } })
+        navigate("/hotels", { state: { destination, dates, options } });
     };
 
     return (
@@ -97,16 +102,16 @@ export const Header = ({ type }) => {
                                 <span
                                     onClick={() => setOpenDate(!openDate)}
                                     className="headerSearchText"
-                                >{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
-                                    date[0].endDate,
+                                >{`${format(dates[0].startDate, "MM/dd/yyyy")} to ${format(
+                                    dates[0].endDate,
                                     "MM/dd/yyyy"
                                 )}`}</span>
                                 {openDate && (
                                     <DateRange
                                         editableDateInputs={true}
-                                        onChange={(item) => setDate([item.selection])}
+                                        onChange={(item) => setDates([item.selection])}
                                         moveRangeOnFirstSelection={false}
-                                        ranges={date}
+                                        ranges={dates}
                                         className="date"
                                         minDate={new Date()}
                                     />
--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -1,44 +1,42 @@
-import React, { useState } from 'react'
-import { Footer } from '../../components/footer/Footer'
-import { Header } from '../../components/header/Header'
-import { MailList } from '../../components/mailLIst/MailList'
-import { Navbar } from '../../components/navbar/Navbar'
-import { AiOutlineLeft } from 'react-icons/ai';
-import { AiOutlineClose } from 'react-icons/ai';
-import { AiOutlineRight } from 'react-icons/ai';
-import { IoLocationSharp } from 'react-icons/io5';
+import React, {useContext, useState} from 'react'
+import useFetch from '../../hooks/useFetch'
+import {useLocation, useNavigate} from 'react-router-dom'
+
+import {Footer} from '../../components/footer/Footer'
+import {Header} from '../../components/header/Header'
+import {MailList} from '../../components/mailLIst/MailList'
+import {Navbar} from '../../components/navbar/Navbar'
+import {AiOutlineLeft} from 'react-icons/ai';
+import {AiOutlineClose} from 'react-icons/ai';
+import {AiOutlineRight} from 'react-icons/ai';
+import {IoLocationSharp} from 'react-icons/io5';
+
 import "./hotel.scss"
-import pic1 from '../../accets/slider/1.jpeg'
-import pic2 from '../../accets/slider/2.jpeg'
-import pic3 from '../../accets/slider/3.jpeg'
-import pic4 from '../../accets/slider/4.jpeg'
-import pic5 from '../../accets/slider/5.jpeg'
-import pic6 from '../../accets/slider/6.jpeg'
+import {SearchContext} from '../../context/SearchContext'
+import {AuthContext} from "../../context/AuthContext";
 
 export const Hotel = () => {
+    const location = useLocation();
+    const id = location.pathname.split("/")[2];
     const [slideNumber, setSlideNumber] = useState(0);
     const [open, setOpen] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
 
-    const photos = [
-        {
-            src: pic1,
-        },
-        {
-            src: pic2,
-        },
-        {
-            src: pic3,
-        },
-        {
-            src: pic4,
-        },
-        {
-            src: pic5,
-        },
-        {
-            src: pic6,
-        },
-    ];
+    const {data, loading, error} = useFetch(`/hotels/find/${id}`);
+    const {user} = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const {dates, options} = useContext(SearchContext);
+
+    const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+
+    function dayDifference(date1, date2) {
+        const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+        const diffDays = Math.ceil(timeDiff / MILLISECONDS_PER_DAY);
+        return diffDays;
+    }
+
+    const days = dayDifference(dates[0].endDate, dates[0].startDate);
 
     const handleOpen = (i) => {
         setSlideNumber(i);
@@ -54,14 +52,21 @@ export const Hotel = () => {
             newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
         }
 
-        setSlideNumber(newSlideNumber)
+        setSlideNumber(newSlideNumber);
     };
 
+    const handleClick = () => {
+        if (user) {
+            setOpenModal(true);
+        } else {
+            navigate("/login");
+        }
+    };
     return (
         <div>
-            <Navbar />
-            <Header type="list" />
-            <div className="hotelContainer">
+            <Navbar/>
+            <Header type="list"/>
+            {loading ? 'Loading...' : <div className="hotelContainer">
                 {open && (
                     <div className="slider">
                         <AiOutlineClose
@@ -73,7 +78,7 @@ export const Hotel = () => {
                             onClick={() => handleMove("l")}
                         />
                         <div className="sliderWrapper">
-                            <img src={photos[slideNumber].src} alt="" className="sliderImg" />
+                            <img src={data.photos[slideNumber]} alt="" className="sliderImg"/>
                         </div>
                         <AiOutlineRight
                             className="arrow"
@@ -83,23 +88,23 @@ export const Hotel = () => {
                 )}
                 <div className="hotelWrapper">
                     <button className="bookNow">Reserve or Book Now!</button>
-                    <h1 className="hotelTitle">Tower Street Apartments</h1>
+                    <h1 className="hotelTitle">{data.name}</h1>
                     <div className="hotelAddress">
-                        <IoLocationSharp />
-                        <span>Elton St 125 New york</span>
+                        <IoLocationSharp/>
+                        <span>{data.address}</span>
                     </div>
                     <span className="hotelDistance">
-                        Excellent location – 500m from center
+                        Excellent location – {data.distance} from center
                     </span>
                     <span className="hotelPriceHighlight">
-                        Book a stay over $114 at this property and get a free airport taxi
+                        Book a stay over ${data.cheapestPrice} at this property and get a free airport taxi
                     </span>
                     <div className="hotelImages">
-                        {photos.map((photo, i) => (
+                        {data.photos?.map((photo, i) => (
                             <div className="hotelImgWrapper" key={i}>
                                 <img
                                     onClick={() => handleOpen(i)}
-                                    src={photo.src}
+                                    src={photo}
                                     alt=""
                                     className="hotelImg"
                                 />
@@ -108,37 +113,27 @@ export const Hotel = () => {
                     </div>
                     <div className="hotelDetails">
                         <div className="hotelDetailsTexts">
-                            <h1 className="hotelTitle">Stay in the heart of City</h1>
+                            <h1 className="hotelTitle">{data.title}</h1>
                             <p className="hotelDesc">
-                                Located a 5-minute walk from St. Florian's Gate in Krakow, Tower
-                                Street Apartments has accommodations with air conditioning and
-                                free WiFi. The units come with hardwood floors and feature a
-                                fully equipped kitchenette with a microwave, a flat-screen TV,
-                                and a private bathroom with shower and a hairdryer. A fridge is
-                                also offered, as well as an electric tea pot and a coffee
-                                machine. Popular points of interest near the apartment include
-                                Cloth Hall, Main Market Square and Town Hall Tower. The nearest
-                                airport is John Paul II International Kraków–Balice, 16.1 km
-                                from Tower Street Apartments, and the property offers a paid
-                                airport shuttle service.
+                                {data.descriptions}
                             </p>
                         </div>
                         <div className="hotelDetailsPrice">
-                            <h1>Perfect for a 9-night stay!</h1>
+                            <h1>Perfect for a {days}-night stay!</h1>
                             <span>
                                 Located in the real heart of Kyiv, this property has an
                                 excellent location score of 9.8!
                             </span>
                             <h2>
-                                <b>$945</b> (9 nights)
+                                <b>${days * data.cheapestPrice * options.room}</b> ({days} nights)
                             </h2>
                             <button>Reserve or Book Now!</button>
                         </div>
                     </div>
                 </div>
-                <MailList />
-                <Footer />
-            </div>
+                <MailList/>
+                <Footer/>
+            </div>}
         </div>
     )
 }
