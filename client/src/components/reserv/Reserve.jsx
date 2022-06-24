60506f890600ae48f963447e49ab838fe03a759d
Artyom <sudakov.artyom@gmail.com>
2022-06-24 19:31:03 +0300

    User can reservation room

--- client/src/components/reserv/Reserve.jsx
+++ client/src/components/reserv/Reserve.jsx
@@ -1,12 +1,89 @@
+import axios from 'axios'
+import { useContext } from 'react'
+import { useState } from 'react'
 import { AiOutlineCloseCircle } from 'react-icons/ai'
+import { SearchContext } from '../../context/SearchContext'
+import { useNavigate } from "react-router-dom";
+import useFetch from '../../hooks/useFetch'
 import './reserve.scss'
 
 const Reserve = ({ setOpen, hotelId }) => {
+    const [selectedRooms, setSelectedRoom] = useState([])
+    const { data, loading, error } = useFetch(`/hotels/room/${hotelId}`)
+    const { dates } = useContext(SearchContext)
+    const navigate = useNavigate()
+
+
+    const getDatesInRange = (startDate, endDate) => {
+
+        const start = new Date(startDate)
+        const end = new Date(endDate)
+        const date = new Date(start.getTime())
+
+        let list = []
+
+        while (date <= end) {
+            list.push(new Date(date).getTime());
+            date.setDate(date.getDate() + 1);
+        }
+
+        return list
+    }
+
+    const alldates = getDatesInRange(dates[0].startDate, dates[0].endDate)
+
+    const isAvailable = (roomNumber) => {
+        const isFound = roomNumber.unavailableDates.some(date => alldates.includes(new Date(date).getTime()))
+
+        return !isFound
+    }
+
+    const handleSelect = (e) => {
+        const checked = e.target.checked
+        const value = e.target.value
+        setSelectedRoom(checked ? [...selectedRooms, value] : selectedRooms.filter(item => item !== value))
+    }
+
+
+    const handleClick = async () => {
+        try {
+            await Promise.all(selectedRooms.map(roomId => {
+                const res = axios.put(`/rooms/availability/${roomId}`, { dates: alldates })
+                return res.data
+            }))
+            setOpen(false)
+            navigate("/")
+        } catch (error) {
+
+        }
+    }
+
     return (
         <div className='reserve'>
             <div className="rContainer">
                 <AiOutlineCloseCircle className='rClose' onClick={() => { setOpen(false) }} />
                 <span>Select your rooms: </span>
+
+                {data.map((item) => (
+                    <div className="rItem">
+                        <div className="rItemInfo">
+                            <div className="rTitle">{item.title}</div>
+                            <div className="rDescription">{item.description}</div>
+                            <div className="rMax"> Max people: <b>{item.maxPeople}</b></div>
+                            <div className="rPrice">{item.price}</div>
+                        </div>
+                        <div className="rSelectRooms">
+                            {item.roomNumbers.map(roomNumber => (
+                                <div className="room">
+                                    <label>{roomNumber.number}</label>
+                                    <input type="checkbox" value={roomNumber._id} onChange={handleSelect} disabled={!isAvailable(roomNumber)} />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                )
+                )}
+                <button className="rButton" onClick={handleClick}>Reserve now!</button>
             </div>
         </div>
     )
